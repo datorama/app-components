8563952eed562425f16bc0839796e875c60bcca5
shay keinan <shay.keinan@datorama.com>
2019-02-27 00:49:29 +0200

    move to create-react-app

--- src/base/Select/Select.types.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const optionsType = PropTypes.arrayOf(
-  PropTypes.shape({
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    label: PropTypes.string
-  }).isRequired
-);
