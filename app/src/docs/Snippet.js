6ea1d640337ff0f5fb5874aee8f386e62a0c049a
shay keinan <shay.keinan@datorama.com>
2019-03-03 15:34:50 +0200

    click to copy code snippet

--- app/src/docs/Snippet.js
+++ app/src/docs/Snippet.js
@@ -1,10 +1,65 @@
 import React from 'react';
 import Highlight from 'react-highlight.js';
+import styled from 'styled-components';
+import {ReactComponent as Copy} from './assets/copy.svg';
+import {withToast} from "../components/base/Toasts";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-export default class Snippet extends React.PureComponent {
-  render() {
-    const { snippet } = this.props;
+const Snippet = props => {
+	const {snippet} = props;
+	
+	return (
+		<Relative>
+			
+			<CopyToClipboard
+				text={snippet}
+				onCopy={() => props.addToast({
+					title: 'Copied successfully!',
+					subtitle: 'Snippet copies to clipboard',
+					type: 'success'
+				})}
+			>
+				<CopyBtn>
+					<StyledCopy/>
+				</CopyBtn>
+			</CopyToClipboard>
+			<Highlight language="javascript">{snippet}</Highlight>
+		</Relative>
+	);
+};
 
-    return <Highlight language="javascript">{snippet}</Highlight>;
+export default withToast(Snippet);
+
+const Relative = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const CopyBtn = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 3px;
+  position: absolute;
+  top: 6px;
+  right: 6px;
+  background: ${({theme}) => theme.p100};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition: all 300ms;
+  
+  &:hover {
+  	background: ${({theme}) => theme.p200};
   }
-}
+`;
+
+const StyledCopy = styled(Copy)`
+	width: 14px;
+	height: 14px;
+	
+	path {
+		fill: ${({theme}) => theme.p300};
+	}
+`;
