0e2f3274dd9f438b3c337519a656d63f3b2c6770
shay keinan <shay.keinan@datorama.com>
2019-04-10 12:09:59 +0300

    update fixed tooltip
    use react portal

--- app/src/docs/Tooltip.doc.js
+++ app/src/docs/Tooltip.doc.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styled, {withTheme} from 'styled-components';
+import styled, { withTheme } from 'styled-components';
 
 // components
 import Base from './Base';
-import {Tooltip, Row, Col} from '../components/index';
+import { Tooltip, Row, Col } from '../components/index';
 import Snippet from './Snippet';
 
 const snippet = `
@@ -35,61 +35,91 @@ const MyComp = ({ navigate }) => (
       title="tooltip content"
       long
       onClickInfo={navigate}
-    />
+    >
+      ...content
+    </Tooltip>
+  </div>
+);
+`;
+
+const snippetFixed = `
+import { Tooltip } from 'app-components';
+
+const MyComp = () => (
+  <div>
+    <Tooltip
+      title="tooltip content"
+      fixed
+    >
+      ...content
+    </Tooltip>
   </div>
 );
 `;
 
 const TooltipDoc = () => {
-	const title = 'tooltip';
-	const description =
-		'Tooltips display informative text when users hover over, focus on, or tap an element.';
-	
-	return (
-		<Base title={title} description={description} name="Tooltip">
-			<Row align="stretch">
-				<Col>
-					<Snippet snippet={snippet}/>
-				</Col>
-				<Col>
-					<Box>
-						<Tooltip title="tooltip content">
-							<Demo>hover me</Demo>
-						</Tooltip>
-					</Box>
-				</Col>
-			</Row>
-			
-			<Row align="stretch">
-				<Col>
-					<Snippet snippet={snippetLeft}/>
-				</Col>
-				<Col>
-					<Box>
-						<Tooltip title="tooltip content" position="LEFT">
-							<Demo>hover me</Demo>
-						</Tooltip>
-					</Box>
-				</Col>
-			</Row>
-			
-			<Row align="stretch">
-				<Col>
-					<Snippet snippet={snippetLong}/>
-				</Col>
-				<Col>
-					<Box>
-						<Tooltip
-							title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam debitis eos harum non, odio placeat porro rem reprehenderit voluptatum"
-							long
-							position="TOP"
-							onClickInfo={() => console.log('clicked!')}
-						/>
-					</Box>
-				</Col>
-			</Row>
-		</Base>
-	);
+  const title = 'tooltip';
+  const description =
+    'Tooltips display informative text when users hover over, focus on, or tap an element.';
+
+  return (
+    <Base title={title} description={description} name="Tooltip">
+      <Row align="stretch">
+        <Col>
+          <Snippet snippet={snippet} />
+        </Col>
+        <Col>
+          <Box>
+            <Tooltip title="tooltip content">
+              <Demo>hover me</Demo>
+            </Tooltip>
+          </Box>
+        </Col>
+      </Row>
+
+      <Row align="stretch">
+        <Col>
+          <Snippet snippet={snippetLeft} />
+        </Col>
+        <Col>
+          <Box>
+            <Tooltip title="tooltip content" position="LEFT">
+              <Demo>hover me</Demo>
+            </Tooltip>
+          </Box>
+        </Col>
+      </Row>
+
+      <Row align="stretch">
+        <Col>
+          <Snippet snippet={snippetLong} />
+        </Col>
+        <Col>
+          <Box>
+            <Tooltip
+              title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam debitis eos harum non, odio placeat porro rem reprehenderit voluptatum"
+              long
+              position="TOP"
+              onClickInfo={() => console.log('clicked!')}
+            />
+          </Box>
+        </Col>
+      </Row>
+
+      <Row align="stretch">
+        <Col>
+          <Snippet snippet={snippetFixed} />
+        </Col>
+        <Col>
+          <Box>
+            <Tooltip title="Lorem ipsum dolor sit amet" fixed>
+              <Rect />
+            </Tooltip>
+          </Box>
+        </Col>
+      </Row>
+    </Base>
+  );
 };
 
 export default withTheme(TooltipDoc);
@@ -101,9 +131,15 @@ const Box = styled.div`
   align-items: center;
   border-radius: 4px;
   justify-content: center;
-  background: ${({theme}) => theme.p50};
+  background: ${({ theme }) => theme.p50};
 `;
 
 const Demo = styled.div`
-  ${({theme}) => theme.text.p};
+  ${({ theme }) => theme.text.p};
+`;
+
+const Rect = styled.div`
+  width: 100px;
+  height: 100px;
+  background: ${({ theme }) => theme.p200};
 `;
