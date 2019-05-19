f10b182d9ca9b75bbb31bd6cbfc493409dc7a417
Daniel <daniel@500tech.com>
2019-05-19 16:32:23 +0300

    [DAT-45162] - Adding groups to Select component WIP

--- app/src/components/base/Select/Select.types.js
+++ app/src/components/base/Select/Select.types.js
@@ -1,5 +1,10 @@
 import PropTypes from 'prop-types';
 
+export const optionsShape = PropTypes.shape({
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  label: PropTypes.string
+});
+
 export const optionsType = PropTypes.arrayOf(
   PropTypes.shape({
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
