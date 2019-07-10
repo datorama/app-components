943bb1a4bf50cb71b4be7286025aa7a6cec7e73f
Shay Keinan <crazypixel@gmail.com>
2019-07-10 16:04:37 +0300

    Update SnailChart.doc.js

--- app/src/docs/SnailChart.doc.js
+++ app/src/docs/SnailChart.doc.js
@@ -1,56 +1,78 @@
 import React from 'react';
-import styled from 'styled-components';
 
 // components
 import Base from './Base';
-import { Row, Col, SnailChart, Card } from '../components/index';
+import { Row, Col } from '../components/index';
 import Snippet from './Snippet';
-import { useTheme } from '../components/hooks/theme.hooks';
 
 const snippet = `
-// inside a component
-import { SnailChart } from '@datorama/app-components';
-
-const data = [
-  { label: '', color: theme.a400, percentage: 50 },
-  { label: '', color: theme.a500, percentage: 60 },
-  { label: '', color: 'red', percentage: 70 }
-];
-
-const MyComp = () => (
-  <div>
-    <SnailChart
-    	linecap="none" // round
-    	dividers={5}
-    	data={data}
-    />
-  </div>
+() => (
+  <SnailChart
+    dividers={5}
+    data={[
+      {
+        percentage: 75,
+        color: theme.a200
+      },
+      {
+        percentage: 63,
+        color: theme.a300
+      },
+      {
+        percentage: 50,
+        color: theme.a400
+      },
+      {
+        percentage: 38,
+        color: theme.a500
+      },
+      {
+        percentage: 80,
+        color: theme.a600
+      },
+      {
+        percentage: 90,
+        color: theme.a700
+      },
+      {
+        percentage: 100,
+        color: theme.a800
+      }
+    ]}
+  />
 );
 `;
 
 const round = `
-// inside a component
-import { SnailChart } from '@datorama/app-components';
-
-const data = [
-  { label: '', color: theme.g400, percentage: 50, hoverColor: theme.g600 },
-  { label: '', color: theme.g500, percentage: 60, hoverColor: theme.g600 },
-  { label: '', color: theme.g500, percentage: 70, hoverColor: theme.g600 }
-];
-
-const MyComp = () => (
-  <div>
-    <SnailChart
-    	linecap="round"
-    	dividers={10}
-    	data={data}
-    />
-  </div>
+() => (
+  <SnailChart
+    dividers={10}
+    linecap="round"
+    data={[
+      {
+        label: 'first label',
+        percentage: 75,
+        color: theme.g300,
+        hoverColor: theme.g600
+      },
+      {
+        label: 'second label',
+        percentage: 63,
+        color: theme.g400,
+        hoverColor: theme.g600
+      },
+      {
+        label: 'third label',
+        percentage: 50,
+        color: theme.g500,
+        hoverColor: theme.g600
+      }
+    ]}
+  />
 );
 `;
 
 const SnailChartDoc = () => {
-  const theme = useTheme();
   const title = 'Snail Chart';
   const description = '';
 
@@ -60,76 +82,11 @@ const SnailChartDoc = () => {
         <Col>
           <Snippet snippet={snippet} />
         </Col>
-        <Col>
-          <StyledCard>
-            <SnailChart
-              dividers={5}
-              data={[
-                {
-                  percentage: 75,
-                  color: theme.a200
-                },
-                {
-                  percentage: 63,
-                  color: theme.a300
-                },
-                {
-                  percentage: 50,
-                  color: theme.a400
-                },
-                {
-                  percentage: 38,
-                  color: theme.a500
-                },
-                {
-                  percentage: 80,
-                  color: theme.a600
-                },
-                {
-                  percentage: 90,
-                  color: theme.a700
-                },
-                {
-                  percentage: 100,
-                  color: theme.a800
-                }
-              ]}
-            />
-          </StyledCard>
-        </Col>
       </Row>
 
       <Row>
         <Col>
           <Snippet snippet={round} />
-        </Col>
-        <Col>
-          <StyledCard>
-            <SnailChart
-              dividers={10}
-              linecap="round"
-              data={[
-                {
-                  label: 'first label',
-                  percentage: 75,
-                  color: theme.g300,
-                  hoverColor: theme.g600
-                },
-                {
-                  label: 'second label',
-                  percentage: 63,
-                  color: theme.g400,
-                  hoverColor: theme.g600
-                },
-                {
-                  label: 'third label',
-                  percentage: 50,
-                  color: theme.g500,
-                  hoverColor: theme.g600
-                }
-              ]}
-            />
-          </StyledCard>
         </Col>
       </Row>
     </Base>
@@ -137,9 +94,3 @@ const SnailChartDoc = () => {
 };
 
 export default SnailChartDoc;
-
-const StyledCard = styled(Card)`
-  align-items: center;
-  justify-content: center;
-  display: flex;
-`;
