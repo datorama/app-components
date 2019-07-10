90fba6848415609c8569c74c1018d2f13bae80a3
Shay Keinan <crazypixel@gmail.com>
2019-07-10 17:07:15 +0300

    Update ClickOut.doc.js

--- app/src/docs/ClickOut.doc.js
+++ app/src/docs/ClickOut.doc.js
@@ -1,87 +1,46 @@
 import React from 'react';
-import styled from 'styled-components';
 
 // components
 import Base from './Base';
-import { Row, Col, ClickOut } from '../components/index';
+import { Col, Row } from '../components/index';
 import Snippet from './Snippet';
 
 const snippet = `
-import { ClickOut } from '@datorama/app-components';
-
-const MyComponent = ({ onClick }) => (
-  <ClickOut onClick={onClick}>
-    sample component
-  </ClickOut>
-);
-`;
-
-export default class ClickOutDoc extends React.Component {
-  state = {
-    counter: 0
+() => {
+  const [counter, setCounter] = useState(0);
+  const advance = useCallback(() => setCounter(counter + 1), [counter]);
+  
+  const styles = {
+    fontSize: 12,
+    color: theme.p600,
+    fontWeight: 500
   };
-
-  handleClick = () =>
-    this.setState(prevState => ({ counter: prevState.counter + 1 }));
-
-  render() {
-    const { counter } = this.state;
-    const title = 'click out';
-    const description = 'click outside handler.';
-
-    return (
-      <Base title={title} description={description} name="ClickOut">
-        <Row>
-          <Col>
-            <Snippet snippet={snippet} />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            <Container>
-              <ClickOut onClick={this.handleClick}>
-                <Card>
-                  <Title>Clicked {counter} times</Title>
-                  <Description>(click outside the element)</Description>
-                </Card>
-              </ClickOut>
-            </Container>
-          </Col>
-        </Row>
-      </Base>
-    );
-  }
-}
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 400px;
-  background: ${({ theme }) => theme.p50};
+  
+  return (
+    <ClickOut onClick={advance}>
+      <Card>
+        <div style={styles}>Clicked {counter} times</div>
+        <div style={styles}>(click outside the element)</div>
+      </Card>
+    </ClickOut>
+  );
+};
 `;
 
-const Card = styled.div`
-	width: 400px;
-	height: 200px;
-	background: ${({ theme }) => theme.p0}
-	box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
-	border-radius: 5px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	font-size: 14px;
-	font-weight: 300;
-	color: #444;
-`;
+const ClickOutDoc = () => {
+  const title = 'click out';
+  const description =
+    'click outside utility. Triggers the onClick callback only when clicking outside the wrapped element.';
 
-const Title = styled.div`
-  ${({ theme }) => theme.text.pBold};
-`;
+  return (
+    <Base title={title} description={description} name="ClickOut">
+      <Row>
+        <Col>
+          <Snippet snippet={snippet} />
+        </Col>
+      </Row>
+    </Base>
+  );
+};
 
-const Description = styled.div`
-  ${({ theme }) => theme.text.pNote};
-`;
+export default ClickOutDoc;
