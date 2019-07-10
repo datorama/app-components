9ee6759f921c6318a58edbe9e27e8f043aad0b98
Shay Keinan <crazypixel@gmail.com>
2019-07-10 22:29:07 +0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -50,7 +50,7 @@ const Navigation = ({ list, history, location, onClick }) => {
     <Fragment>
       <Header>
         <Title onClick={() => history.push('/')}>Apps design system</Title>
-        <Version>0.8.0</Version>
+        <Version>0.8.1</Version>
         <StyledTextInput
           placeholder="search..."
           onChange={e => setTerm(e.target.value)}
