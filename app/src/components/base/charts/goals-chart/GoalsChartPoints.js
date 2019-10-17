1dbe303b1990bf487aa57c7c123dc452d1b4db35
Vladimir Porton <vladimir.porton@datorama.com>
2019-10-17 17:04:45 +0300

    Adjust fill opacity

--- app/src/components/base/charts/goals-chart/GoalsChartPoints.js
+++ app/src/components/base/charts/goals-chart/GoalsChartPoints.js
@@ -61,13 +61,16 @@ const Path = styled.path`
   animation: appear ${({ speed }) => speed}ms linear forwards;
   animation-delay: ${({ speed }) => speed}ms;
   fill: transparent;
+  fill-opacity: 100%;
 
   @keyframes appear {
     from {
       fill: transparent;
+      fill-opacity: 100%;
     }
     to {
-      fill: ${({ color }) => color || 'rgba(255, 255, 255, 0.1)'};
+      fill: ${({ theme, color }) => color || theme.p100};
+      fill-opacity: 10%;
     }
   }
 `;
