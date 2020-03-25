b5244875109cca87b25ae7bba4b4e964a394c9ee
Itay Dafna <idafna@salesforce.com>
2020-03-25 12:21:40 +0200

    adjust to 5 paths

--- app/src/docs/Tornado.doc.js
+++ app/src/docs/Tornado.doc.js
@@ -14,8 +14,7 @@ const snippet = `
     "BDR Prospecting",
     "Event",
     "Account Growth",
-    "Sales Prospecting",
-    "Others"
+    "Sales Prospecting"
   ],
   stats: [
     {
@@ -62,42 +61,33 @@ const snippet = `
       qualifiedValue: "41.5",
       conversionValue: "15.2",
       closeValue: "2.8"
-    },
-    {
-      id: 5,
-      qualified: "Qualified Rate",
-      conversion: "Conversion Rate",
-      close: "Close Rate",
-      qualifiedValue: "36.4",
-      conversionValue: "9.1",
-      closeValue: "1.8"
     }
   ],
   rows: [
     {
       id: "row-0",
-      data: [356, 620, 675, 412, 1447, 110],
+      data: [356, 620, 675, 412, 1447],
       label: "leads created",
       totalValue: 3620,
       totalPercentage: "100"
     },
     {
       id: "row-1",
-      data: [120, 300, 230, 120, 600, 40],
+      data: [120, 300, 230, 120, 600],
       label: "qualified leads",
       totalValue: 1410,
       totalPercentage: "39"
     },
     {
       id: "row-2",
-      data: [60, 80, 40, 40, 220, 10],
+      data: [60, 80, 40, 40, 220],
       label: "leads converted",
       totalValue: 450,
       totalPercentage: "12"
     },
     {
       id: "row-3",
-      data: [5, 12, 9, 5, 40, 2],
+      data: [5, 12, 9, 5, 40],
       label: "closed won",
       totalValue: 73,
       totalPercentage: "2"
