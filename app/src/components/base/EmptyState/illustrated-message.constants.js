325cd1471964101978d1b95b0775c69f04c55708
Vladimir Porton <vladimir.porton@datorama.com>
2020-07-26 10:36:34 +0300

    Switch require with import

--- app/src/components/base/EmptyState/illustrated-message.constants.js
+++ app/src/components/base/EmptyState/illustrated-message.constants.js
@@ -1,3 +1,34 @@
+import noDataLightXS from './assets/no-data/no-data-xs-light.svg';
+import noDataLightS from './assets/no-data/no-data-s-light.svg';
+import noDataLightM from './assets/no-data/no-data-m-light.svg';
+import noDataLightL from './assets/no-data/no-data-l-light.svg';
+import noDataLightXL from './assets/no-data/no-data-xl-light.svg';
+import noDataDarkXS from './assets/no-data/no-data-xs-dark.svg';
+import noDataDarkS from './assets/no-data/no-data-s-dark.svg';
+import noDataDarkM from './assets/no-data/no-data-m-dark.svg';
+import noDataDarkL from './assets/no-data/no-data-l-dark.svg';
+import noDataDarkXL from './assets/no-data/no-data-xl-dark.svg';
+import tooMuchDataLightXS from './assets/too-much-data/too-much-data-xs-light.svg';
+import tooMuchDataLightS from './assets/too-much-data/too-much-data-s-light.svg';
+import tooMuchDataLightM from './assets/too-much-data/too-much-data-m-light.svg';
+import tooMuchDataLightL from './assets/too-much-data/too-much-data-l-light.svg';
+import tooMuchDataLightXL from './assets/too-much-data/too-much-data-xl-light.svg';
+import tooMuchDataDarkXS from './assets/too-much-data/too-much-data-xs-dark.svg';
+import tooMuchDataDarkS from './assets/too-much-data/too-much-data-s-dark.svg';
+import tooMuchDataDarkM from './assets/too-much-data/too-much-data-m-dark.svg';
+import tooMuchDataDarkL from './assets/too-much-data/too-much-data-l-dark.svg';
+import tooMuchDataDarkXL from './assets/too-much-data/too-much-data-xl-dark.svg';
+import errorDarkXS from './assets/error/error-l-dark.svg';
+import errorDarkS from './assets/error/error-l-dark.svg';
+import errorDarkM from './assets/error/error-l-dark.svg';
+import errorDarkL from './assets/error/error-l-dark.svg';
+import errorDarkXL from './assets/error/error-xl-dark.svg';
+import errorLightXS from './assets/error/error-l-light.svg';
+import errorLightS from './assets/error/error-l-light.svg';
+import errorLightM from './assets/error/error-l-light.svg';
+import errorLightL from './assets/error/error-l-light.svg';
+import errorLightXL from './assets/error/error-xl-light.svg';
+
 export const TYPE = {
   NO_DATA: 'NO_DATA',
   TOO_MUCH_DATA: 'TOO_MUCH_DATA',
@@ -20,50 +51,50 @@ export const SIZE = {
 export const ILLUSTRATION = {
   [TYPE.NO_DATA]: {
     [MODE.LIGHT]: {
-      [SIZE.XS]: require('./assets/no-data/no-data-xs-light.svg'),
-      [SIZE.S]: require('./assets/no-data/no-data-s-light.svg'),
-      [SIZE.M]: require('./assets/no-data/no-data-m-light.svg'),
-      [SIZE.L]: require('./assets/no-data/no-data-l-light.svg'),
-      [SIZE.XL]: require('./assets/no-data/no-data-xl-light.svg')
+      [SIZE.XS]: noDataLightXS,
+      [SIZE.S]: noDataLightS,
+      [SIZE.M]: noDataLightM,
+      [SIZE.L]: noDataLightL,
+      [SIZE.XL]: noDataLightXL
     },
     [MODE.DARK]: {
-      [SIZE.XS]: require('./assets/no-data/no-data-xs-dark.svg'),
-      [SIZE.S]: require('./assets/no-data/no-data-s-dark.svg'),
-      [SIZE.M]: require('./assets/no-data/no-data-m-dark.svg'),
-      [SIZE.L]: require('./assets/no-data/no-data-l-dark.svg'),
-      [SIZE.XL]: require('./assets/no-data/no-data-xl-dark.svg')
+      [SIZE.XS]: noDataDarkXS,
+      [SIZE.S]: noDataDarkS,
+      [SIZE.M]: noDataDarkM,
+      [SIZE.L]: noDataDarkL,
+      [SIZE.XL]: noDataDarkXL
     }
   },
   [TYPE.TOO_MUCH_DATA]: {
     [MODE.LIGHT]: {
-      [SIZE.XS]: require('./assets/too-much-data/too-much-data-xs-light.svg'),
-      [SIZE.S]: require('./assets/too-much-data/too-much-data-s-light.svg'),
-      [SIZE.M]: require('./assets/too-much-data/too-much-data-m-light.svg'),
-      [SIZE.L]: require('./assets/too-much-data/too-much-data-l-light.svg'),
-      [SIZE.XL]: require('./assets/too-much-data/too-much-data-xl-light.svg')
+      [SIZE.XS]: tooMuchDataLightXS,
+      [SIZE.S]: tooMuchDataLightS,
+      [SIZE.M]: tooMuchDataLightM,
+      [SIZE.L]: tooMuchDataLightL,
+      [SIZE.XL]: tooMuchDataLightXL
     },
     [MODE.DARK]: {
-      [SIZE.XS]: require('./assets/too-much-data/too-much-data-xs-dark.svg'),
-      [SIZE.S]: require('./assets/too-much-data/too-much-data-s-dark.svg'),
-      [SIZE.M]: require('./assets/too-much-data/too-much-data-m-dark.svg'),
-      [SIZE.L]: require('./assets/too-much-data/too-much-data-l-dark.svg'),
-      [SIZE.XL]: require('./assets/too-much-data/too-much-data-xl-dark.svg')
+      [SIZE.XS]: tooMuchDataDarkXS,
+      [SIZE.S]: tooMuchDataDarkS,
+      [SIZE.M]: tooMuchDataDarkM,
+      [SIZE.L]: tooMuchDataDarkL,
+      [SIZE.XL]: tooMuchDataDarkXL
     }
   },
   [TYPE.ERROR]: {
     [MODE.LIGHT]: {
-      [SIZE.XS]: require('./assets/error/error-l-light.svg'),
-      [SIZE.S]: require('./assets/error/error-l-light.svg'),
-      [SIZE.M]: require('./assets/error/error-l-light.svg'),
-      [SIZE.L]: require('./assets/error/error-xl-light.svg'),
-      [SIZE.XL]: require('./assets/error/error-xl-light.svg')
+      [SIZE.XS]: errorLightXS,
+      [SIZE.S]: errorLightS,
+      [SIZE.M]: errorLightM,
+      [SIZE.L]: errorLightL,
+      [SIZE.XL]: errorLightXL
     },
     [MODE.DARK]: {
-      [SIZE.XS]: require('./assets/error/error-l-dark.svg'),
-      [SIZE.S]: require('./assets/error/error-l-dark.svg'),
-      [SIZE.M]: require('./assets/error/error-l-dark.svg'),
-      [SIZE.L]: require('./assets/error/error-xl-dark.svg'),
-      [SIZE.XL]: require('./assets/error/error-xl-dark.svg')
+      [SIZE.XS]: errorDarkXS,
+      [SIZE.S]: errorDarkS,
+      [SIZE.M]: errorDarkM,
+      [SIZE.L]: errorDarkL,
+      [SIZE.XL]: errorDarkXL
     }
   }
 };
