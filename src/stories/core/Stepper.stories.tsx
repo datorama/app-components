2ca167f0b3e7b99e225ab67e5d92aef9f16cb709
Vladimir Porton <vladimir.porton@datorama.com>
2020-08-09 10:10:03 +0300

    - Add prettier & eslint - Copy tslint config from ECA app - Re-write Stepper with TS

---
+++ src/stories/core/Stepper.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from '@storybook/addons';
+
+// Components
+import Stepper from "packages/core/Stepper/Stepper";
+
+export const DefaultStepper = () => {
+  const [currentStep, setCurrentStep] = useState<number | string>(0);
+
+  return (
+    <Stepper
+      steps={[
+        { id: 0, label: "My brand", isEnabled: true, isTouched: true },
+        { id: 1, label: "Competitors", isEnabled: true },
+        { id: 2, label: "Summary", isEnabled: false },
+      ]}
+      currentStep={currentStep}
+      selectStep={setCurrentStep}
+    />
+  );
+};
+
+export default {
+  title: "Core/Stepper",
+  component: Stepper,
+};
