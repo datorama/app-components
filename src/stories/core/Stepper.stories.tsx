63f2beaeeaa6c268d07ec08f8cad8f337a5f8c41
Vladimir Porton <vladimir.porton@datorama.com>
2020-08-10 09:46:12 +0300

    Add controls addons & add them to Stepper as an example

--- src/stories/core/Stepper.stories.tsx
+++ src/stories/core/Stepper.stories.tsx
@@ -1,33 +1,41 @@
 import React from "react";
-import {
-  withKnobs,
-  object,
-  select,
-} from "@storybook/addon-knobs";
 
 // Components
 import Stepper, { StepperProps } from "packages/core/Stepper/Stepper";
 
 export const Default = (props: StepperProps) => {
-  const value = select('currentStep', [0, 1, 2], 0);
-  const steps = object("steps", [
-    { id: 0, label: "My brand", isEnabled: true, isTouched: true },
-    { id: 1, label: "Competitors", isEnabled: true },
-    { id: 2, label: "Summary", isEnabled: false },
-  ]);
-
   return (
     <Stepper
-      steps={steps}
-      currentStep={value}
-      selectStep={() => {}}
       {...props}
     />
   );
 };
 
+Default.args = {
+  steps: [
+    { id: 0, label: "My brand", isEnabled: true, isTouched: true },
+    { id: 1, label: "Competitors", isEnabled: true },
+    { id: 2, label: "Summary", isEnabled: false },
+  ],
+  currentStep: 0,
+  selectStep: () => {},
+  className: ' '
+};
+
 export default {
   title: "Core/Stepper",
-  component: Stepper,
-  decorators: [withKnobs],
+  argTypes: {
+    steps: {
+      control: 'object',
+    },
+    currentStep: {
+      control: { type: 'number', min: 0, max: 2 }
+    },
+    selectStep: {
+      table: { disable: true }
+    },
+    className: {
+      control: 'text'
+    }
+  }
 };
