a0f5b62fe72d7da34453ad6532064d8648a9d770
Shay Keinan <crazypixel@gmail.com>
2020-08-10 12:25:29 +0300

    Update StyleDecorator.tsx

--- .storybook/StyleDecorator.tsx
+++ .storybook/StyleDecorator.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../src/index.css';
-import {ThemeProvider} from 'styled-components';
+import styled, {ThemeProvider} from 'styled-components';
 import {lightTheme} from '../src/packages/core';
 
 const StyleDecorator = (Story: any) => (
   <ThemeProvider theme={lightTheme}>
-    <Story />
+    <Container>
+      <Story />
+    </Container>
   </ThemeProvider>
 );
 
 export default StyleDecorator;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
