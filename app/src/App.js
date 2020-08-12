cb4d90adbbba6b4685016394e32b61320e946d4f
Itay Dafna <idafna@salesforce.com>
2020-08-12 11:51:49 +0300

    update docs version

--- app/src/App.js
+++ app/src/App.js
@@ -117,7 +117,7 @@ const Navigation = ({ list, history, location, onClick }) => {
     <Fragment>
       <Header>
         <Title onClick={() => history.push('/')}>Apps design system</Title>
-        <Version>0.40.6</Version>
+        <Version>0.41.0</Version>
         <StyledTextInput
           placeholder="search..."
           onChange={e => setTerm(e.target.value)}
