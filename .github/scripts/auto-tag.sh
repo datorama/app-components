299155596f9bdd8758ee189f057c3a340c06b308
Vladimir Porton <vladimir.porton@datorama.com>
2021-06-08 09:59:09 +0300

    Add echo messages for testing

--- .github/scripts/auto-tag.sh
+++ .github/scripts/auto-tag.sh
@@ -13,8 +13,10 @@ TAG_VERSION=$(git tag -l --contains $LAST_COMMIT | cat | tail -1)
 echo "Package version:" $VERSION
 
 if [[ $VERSION == $TAG_VERSION ]]; then
+    echo "Local tag already exists, pushing tag to remote"
     git push origin $VERSION;
 else
+    echo "Local tag doesn't exist, adding a tag & pushing to remote"
     git tag $VERSION
     git push origin $VERSION
 fi
